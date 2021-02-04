7b2246bb6124c84c3c8e6236d6d0b9b2a50096bc
seiwoongchoi <metanonia@outlook.kr>
2021-02-05 00:32:50 +0900

    IntrinsicGas 수정 ..

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -554,6 +554,7 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 		return ErrInsufficientFunds
 	}
 	// Ensure the transaction has more gas than the basic tx fee.
+	/**
 	intrGas, err := IntrinsicGas(tx.Data(), tx.To() == nil, true, pool.istanbul)
 	if err != nil {
 		return err
@@ -561,6 +562,7 @@ func (pool *TxPool) validateTx(tx *types.Transaction, local bool) error {
 	if tx.Gas() < intrGas {
 		return ErrIntrinsicGas
 	}
+	**/
 	return nil
 }
 
